1ec99137e0b0bdcff3748a19e96ba999cdf7674f
shovonkhan200h <shovonkhan00@gmail.com>
2022-11-01 17:46:49 +0600

    add deposite,withdreaw

--- index.js
+++ index.js
@@ -9,18 +9,38 @@ const loginButton=()=>{
 
 
 const deposite=()=>{
-    document.getElementById(`depositeBtn`).addEventListener(`click`, ()=>{
-    let depositeAmount= document.getElementById(`deposite-input`).value;
-    const depositeNumber= +depositeAmount;
-    const currentDeposite= document.getElementById(`currentDeposite`).innerText;
-    const currentDepositeNumber= +currentDeposite;
-    const totalDeposite= depositeNumber+currentDepositeNumber;
-    document.getElementById(`currentDeposite`).innerText= totalDeposite;
+    document.getElementById(`depositeBtn`).addEventListener(`click`, ()=>{ 
+    const depositeNumber= getValue(`deposite-input`);
+    spanTagUpdate(`currentDeposite`,depositeNumber)
+    spanTagUpdate(`currentBalance`,depositeNumber)
     document.getElementById(`deposite-input`).value=``
     })
 }
 
+const spanTagUpdate=(id,depositeNumber)=>{
+    const current= document.getElementById(id).innerText;
+    const currentNumber= +current;
+    const totalAmmount= currentNumber+depositeNumber;
+    document.getElementById(id).innerText= totalAmmount;
+
+}
+
+const withdraw=()=>{
+        document.getElementById(`withdraw`).addEventListener(`click`,()=>{
+        const withdrawNumber= getValue(`withdraw-input`)
+        spanTagUpdate(`withdraw-balance`,withdrawNumber)
+        spanTagUpdate(`currentBalance`,-1*withdrawNumber)
+
+    })
+};
+
+const getValue=(id)=>{
+    let ammount= document.getElementById(id).value;
+    const totalAmmount= +ammount;  
+    return totalAmmount;
+}
 
 
 loginButton()
 deposite()
+withdraw()
